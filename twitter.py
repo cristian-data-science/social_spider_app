d89e35104489f64558d8734ebdce84f6066cca4c
Cristian Gutierrez <cgutierrez.infor@gmail.com>
2022-09-24 00:57:41 -0300

    add link logic to twitter

--- twitter.py
+++ twitter.py
@@ -10,8 +10,10 @@ if __name__ == '__main__':
 
 
     options = webdriver.ChromeOptions()
-    options.headless = True
+    #options.headless = True
     options.add_argument("--log-level=3")
+    options.add_argument("--incognito")
+    options.add_argument("--start-maximized")
     #options.add_argument('proxy-server=106.122.8.54:3128')
     #options.add_argument(r'--user-data-dir=C:\Users\suppo\AppData\Local\Google\Chrome\User Data\Default')
 
@@ -20,8 +22,8 @@ if __name__ == '__main__':
     )
     
     webdriver.get('https://twitter.com/i/trends')
-    sleep(2)
-    xx = webdriver.find_elements(By.XPATH, value= '/html/body/div[1]/div/div/div[2]/main/div/div/div/div[1]/div/div[2]/section/div/div/div[*]/div/div/div')
+    sleep(4)
+    xx = webdriver.find_elements(By.XPATH, value= '/html/body/div[1]/div/div/div[2]/main/div/div/div/div[1]/div/div[*]/section/div/div/div[*]/div/div/div')
     lista = []
     for x in xx:
         linea = x.text  
@@ -34,6 +36,13 @@ if __name__ == '__main__':
     df["tweets"] = df["tweets"].str.replace(".","").str.replace("Tweets","").str.replace(",1 mil","100").str.replace(",1 mil","100").str.replace(",2 mil","200").str.replace(",3 mil","300").str.replace(",4 mil","400").str.replace(",5 mil","500").str.replace(",6 mil","600").str.replace(",7 mil","700").str.replace(",8 mil","800").str.replace(",9 mil","900").str.replace("mil","000").str.replace(" ","")
     df["tweets"] = df["tweets"].str.strip()
     df["tweets"] = df["tweets"].astype(int)
+    links = []
+    for i in df["tendencia"]:
+        links.append(f"https://twitter.com/search?q={i}&src=trend_click&vertical=trends")
+        #print(f"https://twitter.com/search?q={i}&src=trend_click&vertical=trends")
+
+    
+    df["link"] = links
     print(df)
     df.to_csv("./resultados/twitter.csv", index=False)
     
